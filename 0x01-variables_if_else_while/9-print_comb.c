d99d3e194904cee32a501cec73b4fc3257a61915
Ernest <ernestsarfo100@gmail.com>
2023-03-18 02:52:05 +0000

    More errors fixed

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,7 +4,7 @@
   * main - prints all possible combinations of single digit
   * numbers followed by a new line
   *
-  * Return Always 0 (Success)
+  * Return: Always 0 (Success)
   */
 
 int main(void)
