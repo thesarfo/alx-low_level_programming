31df1a9c4d69bb8782bcbeb8947983095cac2a8b
Ernest <ernestsarfo100@gmail.com>
2023-03-18 01:52:14 +0000

    Fixed the betty doc style

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,11 +4,12 @@
  * main - prints the alphabet in lowercase, and then in uppercase
  * followed by a new line
  *
- * Return Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
+
 	int letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
