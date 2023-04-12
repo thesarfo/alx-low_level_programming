476a059c29e1ca3b5baafee05249898e63f55ee7
Ernest <ernestsarfo100@gmail.com>
2023-04-12 01:35:12 +0000

    Added the main.c file

---
+++ 0x0B-malloc_free/0-main.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * simple_print_buffer - print buffer in hexa
+ * @buffer: address of memory to print
+ * @size: size of the memory to print
+ *
+ * Return: Nothing.
+ */
+void simple_print_buffer(char *buffer, unsigned int size)
+{
+    unsigned int i;
+
+    i = 0;
+    while (i < size)
+    {
+        if (i % 10)
+        {
+            printf(" ");
+        }
+        if (!(i % 10) && i)
+        {
+            printf("\n");
+        }
+        printf("0x%02x", buffer[i]);
+        i++;
+    }
+    printf("\n");
+}
+
+/**
+ * main - check code for ALX students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *buffer;
+
+    buffer = create_array(98, 'H');
+    if  (buffer == NULL)
+    {
+        printf("failed to allocate memory\n");
+        return (1);
+    }
+    simple_print_buffer(buffer, 98);
+    free(buffer);
+    return (0);
+}
