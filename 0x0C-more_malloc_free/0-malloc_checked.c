1d32f8c77ee83b049c7fecf21362f7e4781b51e5
Ernest <ernestsarfo100@gmail.com>
2023-04-14 01:52:34 +0000

    allocate memory with malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *malloc_checked - funct that allocates memory with malloc
+ * @b: no of bytes to allocate
+ *
+ * Return: a pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
+
