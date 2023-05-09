08e223d3698f0ae13d6f41ff5a700cdc44722d97
thesarfo <ernestsarfo100@gmail.com>
2023-05-09 00:42:37 +0000

    file change

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,8 @@ int create_file(const char *filename, char *text_content)
 		for (len = 0; text_content[len];)
 			len++;
 	}
-        o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(o, text_content, len);
 
 	if (o == -1 || w == -1)
@@ -31,4 +32,3 @@ int create_file(const char *filename, char *text_content)
 
 	return (1);
 }
-
